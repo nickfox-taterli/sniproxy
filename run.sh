fd454d4b270584cc97332e698d1ff8d71e40c6c6
TaterLi <admin@taterli.com>
2022-02-07 19:44:39 +0800

    添加单栈支持

--- run.sh
+++ run.sh
@@ -3,12 +3,25 @@
 IPV4="203.0.113.0"
 IPV6="2001:db8:ffff:ffff:ffff:ffff:ffff:ffff"
 
+ping -c 4 1.1.1.1 >/dev/null 2>&1
+if [ $? -eq 1 ];then
+    sed -i "s/$IPV4/A/g" /opt/AdGuardHome/AdGuardHome.yaml
+    IPV4="A"
+fi
+
+ping -c 4 2001:4860:4860::8888 >/dev/null 2>&1
+if [ $? -eq 1 ];then
+    sed -i "s/$IPV6/AAAA/g" /opt/AdGuardHome/AdGuardHome.yaml
+    sed -i "s/ipv6_first/ipv4_first/g" /etc/sniproxy.conf
+    IPV6="AAAA"
+fi
+
 # 造一个错误命令来进入循环
 curl 333.333.333.333 2>/dev/null
 while [ $? -ne 0 ] 
 do
     PASSHASH=$(htpasswd -bnBC 10 "" $PASSWORD | tr -d ':\n' | sed 's/$2y/$2a/')
-    sed -i "s/\$2a\$10\$UL51kOUMX6uwmI1y8ddCje.5IQ0.FM4aeTbjC5n3N8Vu9QFZnX.Qq/${PASSHASH}/g" /opt/AdGuardHome/AdGuardHome.yaml
+    sed -i "s/\$2a\$10\$UL51kOUMX6uwmI1y8ddCje.5IQ0.FM4aeTbjC5n3N8Vu9QFZnX.Qq/${PASSHASH}/g" /opt/AdGuardHome/AdGuardHome.yaml >/dev/null 2>&1
 done
 
 while [[ true ]]; do
